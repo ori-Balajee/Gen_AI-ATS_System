90452d5d2b24cd8e2f0347b0fc8bd347290b89a8
ori-Balajee <balajee4drip@gmail.com>
2026-09-15 23:57:32 +0530

    styling login page with scss

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -2,23 +2,25 @@ import "./pages.scss";
 
 function Login() {
   return (
-    <>
-      <div>Login</div>
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
 
-      <form>
-        <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" placeholder="Enter Email Here"/> 
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name='password' placeholder='Enter Password'/>
-        </div>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter Email Here" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name='password' placeholder='Enter Password' />
+          </div>
 
-        <button className="button primary-button">Log In</button>
-      </form>
+          <button className="button primary-button">Log In</button>
+        </form>
 
-    </>
+      </div>
+    </main>
   )
 }
 
