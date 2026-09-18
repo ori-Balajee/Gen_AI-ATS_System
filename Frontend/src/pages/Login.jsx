c89dcdea493d574dd6180d4e69202a090b34c0fd
ori-Balajee <balajee4drip@gmail.com>
2026-09-18 21:54:59 +0530

    Login and Register pages UI

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,12 +1,18 @@
 import "./pages.scss";
+import { Link } from 'react-router-dom'
 
 function Login() {
+
+  function handleReload(e) {
+    e.preventDefault();
+  }
+
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
 
-        <form>
+        <form onSubmit={handleReload}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" placeholder="Enter Email Here" />
@@ -18,6 +24,8 @@ function Login() {
 
           <button className="button primary-button">Log In</button>
         </form>
+
+        <p> Don't have a Account ? <Link to={"/Register"}>Register</Link></p>
 
       </div>
     </main>
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,7 +1,37 @@
+import {Link } from 'react-router-dom'
 
 function Register() {
+
+  function handleReload(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div>Register</div>
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+
+        <form onSubmit={handleReload}>
+          <div className="input-group">
+            <label htmlFor="Username">Username</label>
+            <input type="text" id="Username" name="Username" placeholder="Enter Email Here" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter Email Here" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name='password' placeholder='Enter Password' />
+          </div>
+
+          <button className="button primary-button">Register</button>
+        </form>
+
+        <p> Already have an account ? <Link to={"/Login"}>Login</Link></p>
+
+      </div>
+    </main>
   )
 }
 
